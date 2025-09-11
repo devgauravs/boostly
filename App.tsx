9c4302fc891eb805e462d91cac47978c0e292177
Gaurav <devgaurav400@gmail.com>
2025-09-11 13:03:35 +0530

    updatedCode

--- App.tsx
+++ App.tsx
@@ -4,17 +4,24 @@ import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 
 import RootNavigator from './src/navigation';
-import { store } from './src/redux/store';
+import { persistor, store } from './src/redux/store';
 import Toast from 'react-native-toast-message';
 import { toastConfig } from './src/components/Toast/Toast';
+import { ActivityIndicator } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-      <Toast config={toastConfig} />
+      <PersistGate
+        loading={<ActivityIndicator size="large" color="#0000ff" />} 
+        persistor={persistor}
+      >
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+        <Toast config={toastConfig} />
+      </PersistGate>
     </Provider>
   );
 };
